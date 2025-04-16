5c917c8e8b09cbc4d37f48b79e429c6eb802593a
FaisalMoJ <faisal.amin@justice.gov.uk>
2025-04-16 13:50:02 +0100

    NFDIV-4790: Update eslint config file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,15 +3,7 @@ import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   tseslint.configs.recommended,
-  {
-    ignores: [
-      ".yarn/**",
-      "eslint.config.mjs",
-      ".pnp.cjs",
-      ".pnp.loader.mjs",
-      "format-v4-audit.cjs",
-    ],
-  },
+  { ignores: [".yarn/**", "eslint.config.mjs", ".pnp.cjs", ".pnp.loader.mjs"] },
   {
     files: ["**/*.ts"],
     plugins: {
@@ -25,6 +17,8 @@ export default tseslint.config(
     },
     rules: {
       "@typescript-eslint/no-floating-promises": "error",
+      // This rule should be enabled when enums are converted
+      "@typescript-eslint/no-duplicate-enum-values": "off",
     },
   },
   {
@@ -32,7 +26,12 @@ export default tseslint.config(
     rules: {
       ...playwright.configs["flat/recommended"].rules,
       "playwright/expect-expect": "off",
+      // This should be enabled when waitForSelector calls are changed to locator.waitFor()
+      "playwright/no-wait-for-selector": "off",
+      // This should be enabled when waitForTimeout calls are changed to explicit waits (where possible)
+      "playwright/no-wait-for-timeout": "off",
     },
-    files: ["playwright-e2e/**/*.ts"],
-  }
+    ignores: ["e2e/common/helpers.ts"],
+    files: ["e2e/**/*.ts"],
+  },
 );
