c30152e6e5d75ef51a3ef032a5bda6a8a7262820
FaisalMoJ <faisal.amin@justice.gov.uk>
2025-05-13 14:18:40 +0100

    NFDIV-4792: Fix issue with username and password

--- e2e/common/userHelpers/idamLoginHelper.ts
+++ e2e/common/userHelpers/idamLoginHelper.ts
@@ -65,8 +65,8 @@ export class IdamLoginHelper {
 
     await this.signIn(
       page,
-      userCredentials.email,
-      userCredentials.password,
+      userCredentials.email ?? 'TEST_SOLICITORONLY_USER@mailinator.com',
+      userCredentials.password ?? 'genericPassword123',
       application,
       user,
     );
