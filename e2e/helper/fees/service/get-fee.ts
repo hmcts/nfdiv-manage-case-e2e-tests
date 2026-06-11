cb407bdf091307ece356f7bc20713fc100e05498
FaisalMoJ <100577377+FaisalMoJ@users.noreply.github.com>
2026-06-11 15:47:30 +0100

    Apply suggestion from @adamg-hmcts
    Co-authored-by: adamg-hmcts <159780146+adamg-hmcts@users.noreply.github.com>

--- e2e/helper/fees/service/get-fee.ts
+++ e2e/helper/fees/service/get-fee.ts
@@ -6,16 +6,16 @@ dotenv.config();
 
 const logger = Logger.getLogger('get-fees');
 const fees = {
-  DivorceCivPart: { amount: 0, service: 'divorce', event: 'issue' },
-  DivorceAmendPetition: { amount: 0, service: 'other', event: 'issue' },
-  AppnPrivateOther: { amount: 0, service: 'other', event: 'issue' },
-  GAContestedOrder: { amount: 0, service: 'other', event: 'general application' },
-  BailiffServeDoc: { amount: 0, service: 'other', event: 'enforcement' },
-  FinancialOrderOnNotice: { amount: 0, service: 'other', event: 'miscellaneous' },
-  GeneralAppWithoutNotice: { amount: 0, service: 'other', event: 'general application' },
-  GAOnNotice: { amount: 0, service: 'other', event: 'general application' },
-  DivAnswerReceived: { amount: 0, service: 'other', event: 'issue' },
-  SearchIndexDivCentral: { amount: 0, service: 'other', event: 'search' },
+  DivorceCivPart: { amount: null, service: 'divorce', event: 'issue' },
+  DivorceAmendPetition: { amount: null, service: 'other', event: 'issue' },
+  AppnPrivateOther: { amount: null, service: 'other', event: 'issue' },
+  GAContestedOrder: { amount: null, service: 'other', event: 'general application' },
+  BailiffServeDoc: { amount: null, service: 'other', event: 'enforcement' },
+  FinancialOrderOnNotice: { amount: null, service: 'other', event: 'miscellaneous' },
+  GeneralAppWithoutNotice: { amount: null, service: 'other', event: 'general application' },
+  GAOnNotice: { amount: null, service: 'other', event: 'general application' },
+  DivAnswerReceived: { amount: null, service: 'other', event: 'issue' },
+  SearchIndexDivCentral: { amount: null, service: 'other', event: 'search' },
 };
 
 type FeeKeyword = keyof typeof fees;
@@ -39,18 +39,10 @@ export const updateFee = async (keyword: FeeKeyword): Promise<void> => {
   }
 };
 
-const updateFees = async (): Promise<void> => {
-  logger.info('Refreshing fees');
-  await Promise.all(Object.keys(fees).map(k => updateFee(k as FeeKeyword)));
-};
-
-export const initFees = async (): Promise<void> => {
-  await updateFees();
-  setInterval(updateFees, 1000 * 60 * 60);
-};
-
 export const getFee = async (keyword: FeeKeyword): Promise<string> => {
+  if (!fees[keyword].amount) {
+    updateFee(keyword);
+  }
+  
   return '£' + fees[keyword].amount;
 };
-
-await initFees();
